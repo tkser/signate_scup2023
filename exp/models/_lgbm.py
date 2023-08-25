f3141f5989a6eb8b8990f047c19aa857ca64e46f
tkser <tkser.edu@gmail.com>
2023-08-25 06:42:37 +0000

    fix: turning

--- exp/models/_lgbm.py
+++ exp/models/_lgbm.py
@@ -32,17 +32,16 @@ class LGBMModel:
     
     def _objective_trial(self, trial: optuna.Trial) -> float:
         params = {
-            "num_leaves": trial.suggest_int("num_leaves", 2, 64),
-            "max_depth": trial.suggest_int("max_depth", 2, 24),
-            "min_child_samples": trial.suggest_int("min_child_samples", 1, 128),
-            "subsample": trial.suggest_float("subsample", 0.1, 1.0),
-            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.1, 1.0),
-            "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 1.0, log=True),
-            "reg_lambda": trial.suggest_float("reg_lambda", 1e-8, 1.0, log=True),
-            "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
-            "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
-            "bagging_freq": trial.suggest_categorical("bagging_freq", [1, 5]),
+            "num_leaves": trial.suggest_int("num_leaves", 2, 40),
+            "max_depth": trial.suggest_int("max_depth", 2, 12),
+            "min_child_samples": trial.suggest_int("min_child_samples", 2, 64),
+            "subsample": trial.suggest_float("subsample", 0.1, 0.5),
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.1, 0.5),
+            "reg_alpha": trial.suggest_float("reg_alpha", 1e-3, 10.0, log=True),
+            "reg_lambda": trial.suggest_float("reg_lambda", 1e-3, 10.0, log=True),
+            "subsample_freq": trial.suggest_categorical("subsample_freq", [0, 1, 2, 5, 10]),
         }
+        kf = KFold(n_splits=5, shuffle=True, random_state=self.seed * 2)
         X, y = self.X_train.clone().to_numpy(), self.y_train.clone().to_numpy()
         X_cv, X_eval, y_cv, y_eval = train_test_split(X, y, test_size=0.25, random_state=self.seed, shuffle=True)
         model = LGBMRegressor(
@@ -56,15 +55,20 @@ class LGBMModel:
             verbose=-1,
             n_jobs=-1
         )
-        model.fit(
+        scores = cross_val_score(
+            model, # type: ignore
             X_cv, y_cv,
-            eval_set=[(X_cv, y_cv), (X_eval, y_eval)],
-            callbacks=[early_stopping(200, verbose=False), log_evaluation(0)],
-            eval_metric="mape"
+            scoring=make_scorer(mean_absolute_percentage_error, greater_is_better=False),
+            cv=kf,
+            n_jobs=-1,
+            verbose=0,
+            fit_params={
+                "eval_set": [(X_eval, y_eval)],
+                "callbacks": [early_stopping(200, verbose=False), log_evaluation(0)],
+                "eval_metric": "mape"
+            }
         )
-        y_pred = model.predict(X_eval, num_iteration=model.best_iteration_)
-        score = mean_absolute_percentage_error(y_eval, y_pred) # type: ignore
-        return score
+        return -scores.mean()
     
     def objective(self, n_trial = 100) -> dict:
         study = optuna.create_study(sampler=optuna.samplers.TPESampler(seed=self.seed), direction="minimize")
@@ -76,6 +80,31 @@ class LGBMModel:
         self.models = []
         predictions = pl.DataFrame(np.zeros((self.X_all.shape[0], n_splits)), schema=[f"{col_name}_pred_{i}" for i in range(n_splits)])
         X, y = self.X_train.to_numpy(), self.y_train.to_numpy()
+        if n_splits == 1:
+            X_cv, X_eval, y_cv, y_eval = train_test_split(X, y, test_size=0.25, random_state=self.seed, shuffle=True)
+            model = LGBMRegressor(
+                **self.best_params,
+                random_state=self.seed,
+                n_estimators=10000,
+                metric="mape",
+                objective=self.objective_param,
+                importance_type="gain",
+                learning_rate=0.01,
+                verbose=-1
+            )
+            model.fit(
+                X_cv, y_cv,
+                eval_set=[(X_eval, y_eval)],
+                callbacks=[early_stopping(100, verbose=False), log_evaluation(0)],
+                eval_metric="mape"
+            )
+            self.models.append(model)
+            y_pred_all = model.predict(self.X_all)
+            predictions = predictions.with_columns(
+                pl.Series(y_pred_all).alias(f"{col_name}_pred_0"),
+            )
+            print(f"Fold_{col_name} 0: {mean_absolute_percentage_error(y_eval, model.predict(X_eval))}") # type: ignore
+            return predictions
         kf = KFold(n_splits=n_splits, shuffle=True, random_state=self.seed)
         scores = []
         for i, (train_index, valid_index) in enumerate(kf.split(X, y)):
--- main.py
+++ main.py
@@ -48,24 +48,55 @@ def main():
                 lgbm01_predictions = lgbm01.predict(5, f"lgbm01_{random_status}")
                 predictions_all = pl.concat([predictions_all, lgbm01_predictions], how="horizontal")
 
-            if not LGBM_ONLY:
-                with Timer(f"lgbm02_{random_status}"):
-                    lgbm02 = LGBMModel(*selecter.get_dataframe("lgbm"), seed=random_status)
-                    lgbm02.best_params = {
-                        'num_leaves': 41,
-                        'max_depth': 4,
-                        'min_child_samples': 65,
-                        'subsample': 0.1813266686908916,
-                        'colsample_bytree': 0.9997207808739403,
-                        'reg_alpha': 3.8163343968470076e-06,
-                        'reg_lambda': 9.185674902594394e-05,
-                        'feature_fraction': 0.5180973927754882,
-                        'bagging_fraction': 0.8804646505719466,
-                        'bagging_freq': 1
-                    }
-                    lgbm02_predictions = lgbm02.predict(4, f"lgbm02_{random_status}")
-                    predictions_all = pl.concat([predictions_all, lgbm02_predictions], how="horizontal")
+            with Timer(f"lgbm02_{random_status}"):
+                lgbm02 = LGBMModel(*selecter.get_dataframe("lgbm"), seed=random_status)
+                lgbm02.best_params = {
+                    'num_leaves': 41,
+                    'max_depth': 4,
+                    'min_child_samples': 65,
+                    'subsample': 0.1813266686908916,
+                    'colsample_bytree': 0.9997207808739403,
+                    'reg_alpha': 3.8163343968470076e-06,
+                    'reg_lambda': 9.185674902594394e-05,
+                    'feature_fraction': 0.5180973927754882,
+                    'bagging_fraction': 0.8804646505719466,
+                    'bagging_freq': 1
+                }
+                lgbm02_predictions = lgbm02.predict(4, f"lgbm02_{random_status}")
+                predictions_all = pl.concat([predictions_all, lgbm02_predictions], how="horizontal")
+            
+            with Timer(f"lgbm03_{random_status}"):
+                lgbm03 = LGBMModel(*selecter.get_dataframe("lgbm"), seed=random_status)
+                lgbm03.best_params = {
+                    'num_leaves': 21,
+                    'max_depth': 2,
+                    'min_child_samples': 45,
+                    'subsample': 0.1813266686908916,
+                    'colsample_bytree': 0.9997207808739403,
+                    'reg_alpha': 3.8163343968470076e-06,
+                    'reg_lambda': 9.185674902594394e-05,
+                    'feature_fraction': 0.5180973927754882,
+                    'bagging_fraction': 0.8804646505719466,
+                    'bagging_freq': 5
+                }
+                lgbm03_predictions = lgbm03.predict(5, f"lgbm03_{random_status}")
+                predictions_all = pl.concat([predictions_all, lgbm03_predictions], how="horizontal")
+            
+            with Timer(f"lgbm04_{random_status}"):
+                lgbm04 = LGBMModel(*selecter.get_dataframe("lgbm"), seed=random_status)
+                lgbm04.best_params = {
+                    'num_leaves': 60,
+                    'min_child_samples': 128,
+                    'subsample': 0.4557739655602947,
+                    'colsample_bytree': 0.4544303943274487,
+                    'reg_alpha': 0.00018337518898873693,
+                    'reg_lambda': 0.06367058193742765,
+                    'subsample_freq': 1
+                }
+                lgbm04_predictions = lgbm04.predict(1, f"lgbm04_{random_status}")
+                predictions_all = pl.concat([predictions_all, lgbm04_predictions], how="horizontal")
 
+            if not LGBM_ONLY:
                 with Timer(f"xgb_{random_status}"):
                     xgb = XGBModel(*selecter.get_dataframe("xgb"))
                     #xgb.objective(20)
@@ -95,7 +126,7 @@ def main():
 
         sub = pl.read_csv(os.path.join(os.path.dirname(__file__), "input/submit_sample.csv"), has_header=False, new_columns=["id", "price"])
         sub = sub.with_columns(pl.Series("", y_pred).alias("price"))
-        sub.write_csv(os.path.join(os.path.dirname(__file__), "output/submission_te0819_1.csv"), has_header=False)
+        sub.write_csv(os.path.join(os.path.dirname(__file__), "output/submission_te0823_1.csv"), has_header=False)
 
 
 if __name__ == "__main__":
